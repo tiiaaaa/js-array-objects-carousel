8b335cc374272d0593784fa6f0add90c93c21e19
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-10 19:04:08 +0100

    completamento consegna base del esercizio

--- js/script.js
+++ js/script.js
@@ -1,26 +1,4 @@
-// const items = [
-//     'img/01.jpg',
-//     'img/02.jpg',
-//     'img/03.jpg',
-//     'img/04.jpg',
-//     'img/05.jpg'
-// ];
 
-// const title = [
-//     'Svezia',
-//     'Svizzera',
-//     'Gran Bretagna',
-//     'Germania',
-//     'Paradise'
-// ]
-
-// const text = [
-//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-//     'Lorem ipsum',
-//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-//     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-// ]
 
 const objectArray = [
     {
@@ -61,17 +39,62 @@ for (let i = 0; i < objectArray.length; i = i + 1){
     sliderContent += `
     <div class="my-carousel">
         <img class="my-carousel-picture" src="${objectArray[i].items}" alt="${objectArray[i].title}">
-        <div class="my-carousel-description">
+        <div class="my-carousel-description position-absolute bottom-0 end-0 text-end text-white">
             <h2>${objectArray[i].title}</h2>
             <p>${objectArray[i].text}</p>
         </div>
     </div>`
-    console.log(sliderContent)
+
+    thumbnailsContent += `
+    <div class="my-thumbnail">
+        <img class="my-thumbnail-img" src="${objectArray[i].items}" alt="${objectArray[i].title}">
+    </div>`
 }
 
+const sliderElement = document.querySelector('.my-carousel-images');
+sliderElement.classList.add("position-relative");
+sliderElement.innerHTML = sliderContent;
+
+const thumbnailsElement = document.querySelector('.my-thumbnails');
+thumbnailsElement.classList.add("d-flex");
+thumbnailsElement.innerHTML += thumbnailsContent;
+
+let activeElement = 0;
+
+document.getElementsByClassName("my-carousel")[activeElement].classList.add("active");
+
+document.getElementsByClassName("my-thumbnail")[activeElement].classList.add("active");
+
+const nextBtn = document.querySelector("div.my-next");
+nextBtn.addEventListener("click", function(){
+    document.getElementsByClassName("my-carousel")[activeElement].classList.remove("active");
+    document.getElementsByClassName("my-thumbnail")[activeElement].classList.remove("active");
+    
+    if(activeElement == objectArray.length - 1){
+        activeElement = 0
+    }else{
+        activeElement++;
+    }
+
+    document.getElementsByClassName("my-carousel")[activeElement].classList.add("active");
+    document.getElementsByClassName("my-thumbnail")[activeElement].classList.add("active");
+});
 
 
+const prevBtn = document.querySelector("div.my-previous");
+prevBtn.addEventListener("click", function(){
+    document.getElementsByClassName("my-carousel")[activeElement].classList.remove("active");
+    document.getElementsByClassName("my-thumbnail")[activeElement].classList.remove("active");
+    
+    if(activeElement == 0){
+        activeElement = objectArray.length - 1
+    }else{
+        activeElement--;
+    }
 
+    document.getElementsByClassName("my-carousel")[activeElement].classList.add("active");
+    document.getElementsByClassName("my-thumbnail")[activeElement].classList.add("active");
+})
 
 
 
