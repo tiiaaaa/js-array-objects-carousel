ca39143c2df7dee2dcb7f66dfefdea9645e46f85
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-10 16:20:52 +0100

    impostazione file di lavoro e creazione di array di oggetti

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+const objectArray = [
+    {
+        items:'img/01.jpg',
+        title:'Svezia',
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+
+    {
+        items:'img/02.jpg',
+        title:'Svizzera',
+        text:'Lorem ipsum',
+    },
+
+    {
+        items:'img/03.jpg',
+        title:'Gran Bretagna',
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+
+    {
+        items:'img/04.jpg',
+        title:'Germania',
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    },
+
+    {
+        items:'img/05.jpg',
+        title:'Paradise',
+        text:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    },
+];
+
+console.log(objectArray);
+
+
+
+
+
+
+
+
