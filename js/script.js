f6b62211d8578536fef4ad0b8e2bbc85f8138313
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-10 17:35:08 +0100

    izio file javascript

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
+// const items = [
+//     'img/01.jpg',
+//     'img/02.jpg',
+//     'img/03.jpg',
+//     'img/04.jpg',
+//     'img/05.jpg'
+// ];
 
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
+// const title = [
+//     'Svezia',
+//     'Svizzera',
+//     'Gran Bretagna',
+//     'Germania',
+//     'Paradise'
+// ]
 
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+// const text = [
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+//     'Lorem ipsum',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+//     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+// ]
 
 const objectArray = [
     {
@@ -54,7 +54,20 @@ const objectArray = [
     },
 ];
 
-console.log(objectArray);
+let sliderContent = "";
+let thumbnailsContent = "";
+
+for (let i = 0; i < objectArray.length; i = i + 1){
+    sliderContent += `
+    <div class="my-carousel">
+        <img class="my-carousel-picture" src="${objectArray[i].items}" alt="${objectArray[i].title}">
+        <div class="my-carousel-description">
+            <h2>${objectArray[i].title}</h2>
+            <p>${objectArray[i].text}</p>
+        </div>
+    </div>`
+    console.log(sliderContent)
+}
 
 
 
